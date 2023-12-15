81f139567e3aa656127484f8962179294b33bf44
Pavel <magma4810@gmail.com>
2023-12-16 00:14:32 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: "public/index.html",
     }),
     new BrowserSyncPlugin({
       host: "localhost",
